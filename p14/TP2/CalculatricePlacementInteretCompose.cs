695cd96723980c7fe2c244124ba8d93326c4988f
DelfCS <101904674+DelfCS@users.noreply.github.com>
2023-09-26 20:55:55 -0400

    Adding session 1 projects

---
+++ p14/TP2/CalculatricePlacementInteretCompose.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using static System.Console;
+
+namespace CalculatricePlacement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            const string INSTRUC_DEPOT_INITIAL = "Veuillez entrer le montant du dépot initial";
+
+            const string INSTRUC_INTERET_ANNUEL = "Veuillez entrer le taux d'interet annuel ( Valeur en pourcentage )";
+
+            const string INSTRUC_DEPOT_MENSUEL = "Veuillez entrer le montant des depots subséquents mensuels";
+            
+
+            double depotInitial = default;
+
+            double tauxInteretAnnuel = default;
+
+            double depotMensuel = default;
+
+            double montantEconomie = default;
+
+            double nbrMoisMillionaire = default;
+
+            double milion = 1000000;
+
+            double interetTotal = default;
+
+            double anneeMillionaire = default;
+
+            double moisExtraMill = default;
+
+
+            string investissementTotalFormat;
+
+            string interetTotalFormat = default;
+
+            string depotInitialFormat = default;
+
+            string tauxInteretFormat = default;
+
+            string depotMensuelFormat = default;
+
+            string montantEconomieFormat = default;
+
+            string ouiOuNon = "N";
+            
+
+            bool millionaire = false;
+
+            bool valide = false;
+
+            bool continuer = true;
+
+
+
+            while(continuer){
+
+                valide = false;
+
+                millionaire = false;
+
+                nbrMoisMillionaire = default;
+
+
+                WriteLine(INSTRUC_DEPOT_INITIAL);
+
+                while(!valide){
+                    valide = double.TryParse(ReadLine(), out depotInitial) && depotInitial > 0;
+                    if(!valide){
+                        WriteLine("La valeur entrée doit être un nombre suppérieur à zéro");
+                    }
+                }
+
+                depotInitialFormat = depotInitial.ToString("C");
+                depotInitialFormat += ("\n");
+                WriteLine(depotInitialFormat);
+                
+
+                valide = false;
+
+                WriteLine(INSTRUC_INTERET_ANNUEL);
+
+                while(!valide){
+                    valide = double.TryParse(ReadLine(), out tauxInteretAnnuel) && tauxInteretAnnuel > 0 && tauxInteretAnnuel < 100;
+                    if(!valide){
+                        WriteLine("La valeur doit être un nombre situé entre 0 & 100");
+                    }
+                }
+
+                tauxInteretFormat = tauxInteretAnnuel.ToString();
+                tauxInteretFormat += ("%\n");
+                WriteLine(tauxInteretFormat);
+
+
+                valide = false;
+
+                WriteLine(INSTRUC_DEPOT_MENSUEL);
+
+                while(!valide){
+                    valide = double.TryParse(ReadLine(), out depotMensuel) && depotMensuel > 0;
+                    if(!valide){
+                        WriteLine("Les dépots mensuels doivent être un nombre plus grand que zéro");
+                    }
+                }
+
+                depotMensuelFormat = depotMensuel.ToString("C");
+                depotMensuelFormat += ("\n");
+                WriteLine(depotMensuelFormat);
+
+
+                montantEconomie = depotInitial;
+
+                while(!millionaire){
+                    nbrMoisMillionaire ++;
+                    montantEconomie += depotMensuel;
+                    montantEconomie = montantEconomie * (1+(tauxInteretAnnuel/12/100));
+                    millionaire = montantEconomie >= milion;
+                }
+
+
+                montantEconomieFormat = montantEconomie.ToString("C");
+
+
+                investissementTotalFormat = (depotMensuel * nbrMoisMillionaire + depotInitial).ToString("C");
+
+
+                interetTotal = default;
+                interetTotal = (montantEconomie - depotInitial - (depotMensuel * nbrMoisMillionaire));
+                interetTotalFormat = interetTotal.ToString("C");
+
+
+                anneeMillionaire = Math.Floor(nbrMoisMillionaire / 12);
+                moisExtraMill = nbrMoisMillionaire % 12; 
+
+
+
+                WriteLine("\n Depot Initial               : {0} Nombre de mois millionaire  : {1} \n Total de l'investissement   : {2} \n Économies total à la fin    : {3} \n Interets total gagnés       : {4}\n\n",
+                depotInitialFormat, nbrMoisMillionaire, investissementTotalFormat, montantEconomieFormat, interetTotalFormat );
+
+                WriteLine($"Donc, il vous faudra {anneeMillionaire} années & {moisExtraMill} mois pour devenir millionaire, merde à vous !");
+
+               
+                WriteLine("\n\nVoulez vous continuer ? (O) ou (o) pour oui, (N) ou (n) pour non, NON par défault");
+                ouiOuNon = ReadLine();
+                continuer = ouiOuNon == "O" || ouiOuNon == "o";
+
+            }
+        }
+    }
+}
---
+++ p14/TP2/CalculatricePretHypotecaire.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using static System.Console;
+namespace CalculatricePretHypotecaire
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            bool continuer = true;
+
+            bool valide = false;
+
+
+            const string MSG_INSTRUC_MONTANT = "\nEntrez le montant de votre prêt";
+
+            const string MSG_INSTRUC_TAUX = "\nEntrez le taux d'intérêts ( Valeur calculée sur 100 )";
+
+            const string MSG_INSTRUC_PERIODE_AMORT = "\nEntrez votre période d'amortissement : 5, 10, 15, 20, 25 ou 30 ans";
+           
+            const string MSG_INSTRUC_RECOMMENCER = "\n\nSouhaitez-vous recommencer ? (O) pour Oui, (N) pour Non, ( Default = O )"; 
+
+
+            string montantPretFormat = "";
+
+            string versementMensuelFormat = ""; 
+
+            string utilContinuer = default;
+
+
+            double montantPret = default;
+
+            double tauxInterets = default;
+
+            double periodeAmortissement = default;
+
+            double versementMensuel = default;
+
+
+            while(continuer){
+
+                valide = false;
+
+
+                while(!valide){
+                    WriteLine(MSG_INSTRUC_MONTANT);
+                    valide = double.TryParse(ReadLine(), out montantPret);
+                    montantPretFormat = montantPret.ToString("C");    
+                }
+                WriteLine($"Le montant de votre prêt est de {montantPretFormat}"); 
+
+
+                valide = false;   
+
+
+                while(!valide){
+                    WriteLine(MSG_INSTRUC_TAUX);
+                    valide = double.TryParse(ReadLine(), out tauxInterets);
+                }               
+                WriteLine($"Votre taux d'interêts sera de {tauxInterets}%");
+ 
+
+                valide = false;
+
+
+                while(!valide){
+                    WriteLine(MSG_INSTRUC_PERIODE_AMORT);
+                    double.TryParse(ReadLine(), out periodeAmortissement);
+                    valide = periodeAmortissement % 5 == 0 && periodeAmortissement < 31;  
+                }
+                WriteLine($"Votre période d'amortissement sera de {periodeAmortissement} ans");
+
+ 
+                versementMensuel = montantPret * ( tauxInterets / 100 ) / 12; 
+                versementMensuel = versementMensuel / ( 1 - Math.Pow( 1 + (( tauxInterets / 100 ) / 12), ( -12 * periodeAmortissement) ));
+
+                versementMensuelFormat = versementMensuel.ToString("C");
+                WriteLine($"\n\nVotre versement mensuel sera de {versementMensuelFormat}");
+
+
+                WriteLine(MSG_INSTRUC_RECOMMENCER);
+                
+                utilContinuer = ReadLine();
+
+                if(utilContinuer == "N" || utilContinuer == "n" ){
+                    continuer = false;
+                }
+            }
+
+        }
+    }
+}
